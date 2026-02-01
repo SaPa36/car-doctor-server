eb7e530a3f671df09fd2340c37426901cf7c4bd2
SaPa36 <nursapa2001@gmail.com>
2026-02-01 12:23:47 +0600

    get specific id specific data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const servicesCollection = client.db('carDoctorDB').collection('services');
+
+        app.get('/services', async (req, res) => {
+            
+            const cursor = servicesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options ={
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            }
+            const result = await servicesCollection.findOne(query, options);
+            res.send(result);
+        });
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
