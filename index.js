25a1cb60fcbd13821619e7e697051add393849c3
SaPa36 <nursapa2001@gmail.com>
2026-02-01 09:37:15 +0600

    create car doctor server and runing server

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Car Doctor Server is running');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
