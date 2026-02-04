ac44a63816cda58da6d86bb7a905a4f55b439b77
SaPa36 <nursapa2001@gmail.com>
2026-02-04 16:24:18 +0600

    deploy in vercel successfully and data loaded

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        //'http://localhost:5173',
+        'https://car-doctor-7e43e.web.app',
+        'https://car-doctor-7e43e.firebaseapp.com',
+        //'https://car-doctor-server-lemon-two.vercel.app'
     ],
     credentials: true
 }));
@@ -34,6 +37,12 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+//custom middleware to verify JWT
+const logger = (req, res, next) => {
+    console.log('Logged In User:', req.method, req.url);
+    next();
+};
 
 const verifyJWT = (req, res, next) => {
     // Read the token from cookies instead of headers
@@ -55,7 +64,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         const servicesCollection = client.db('carDoctorDB').collection('services');
         const bookingsCollection = client.db('carDoctorDB').collection('bookings');
 
@@ -74,7 +83,7 @@ async function run() {
                     sameSite: 'none',
 
                 })
-                .send({ success: true, token });
+                .send({ success: true});
         });
 
         app.post('/logout', async (req, res) => {
@@ -104,10 +113,13 @@ async function run() {
 
 
         // Bookings API
-        app.get('/bookings', verifyJWT,  async (req, res) => {
+        app.get('/bookings', logger, verifyJWT,  async (req, res) => {
             console.log(req.query.email);
             console.log(req.cookies.token); 
             console.log('token owner info', req.decoded);
+            if (req.decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' });
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -159,7 +171,13 @@ app.get('/', (req, res) => {
     res.send('Car Doctor Server is running');
 });
 
-// Start server
-app.listen(port, () => {
+
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});
+  });
+}
+
+
+module.exports = app;
